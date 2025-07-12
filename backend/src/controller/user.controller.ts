651261e489877d2278c59cd8647054a738d89b13
Pranjal Mantri <pranjalmantri@gmail.com>
2025-07-12 12:02:06 +0530

    feat: add 'me' endpoint to retrieve user profile information

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -1,7 +1,6 @@
 import User from "../models/user.model.ts";
 import { signinSchema, signupSchema } from "../schema/user.schema.ts";
 import { Request, Response } from "express";
-import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const signup = async (req: Request, res: Response) => {
@@ -85,4 +84,26 @@ const signin = async (req: Request, res: Response) => {
   return res.status(200).json({ user: userResponse, token });
 };
 
-export { signup, signin };
+const me = async (req: Request, res: Response) => {
+  const userId = req.user?.id;
+
+  if (!userId) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  const userResponse = {
+    id: user._id,
+    email: user.email,
+    fullName: user.fullName,
+  };
+
+  return res.status(200).json({ user: userResponse });
+};
+
+export { signup, signin, me };
--- backend/src/routes/auth.router.ts
+++ backend/src/routes/auth.router.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
-import { signup, signin } from "../controller/user.controller.ts";
+import { signup, signin, me } from "../controller/user.controller.ts";
+import authMiddleware from "../middleware/authMiddleware.ts";
 
 const app = express();
 
 app.post("/signin", signin);
 app.post("/signup", signup);
-app.get("/me", (req: Request, res: Response) => {});
+app.get("/me", authMiddleware, me);
 
 export default app;
