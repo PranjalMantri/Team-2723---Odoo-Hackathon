ebc5a01aeef7a97b6f1542b385d8219313367e73
580dhruv <dhruvishpatel580@gmail.com>
2025-07-12 14:35:35 +0530

    ItemDetail page completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import ListItemForm from './components/ListItemForm';
 import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm'
+import ItemDetail from './components/ItemDetail';
 
 const App=()=> {
+  const item = {
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFYqoKTu_o3Zns2yExbst2Co84Gpc2Q1RJbA&s',
+    title: 'Retro Graphic Tee',
+    user: 'Jay Mehta',
+    category: 'Men’s Clothing',
+    description: 'Vintage-inspired graphic t-shirt made from soft, breathable cotton. Lightly used and well-maintained.',
+    points: 30,
+    listingType: 'Swap',
+  }
+  
+  
   return(
     <>
-    <LoginForm/>
+    <ItemDetail item={item}/>
     </>
   )
 }
